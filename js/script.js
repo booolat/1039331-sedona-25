ac8b93b78e733584c32ec5fb0fa420e5b04c8b53
user <user@users-iMac.local>
2019-06-26 17:36:05 +0300

    Финальные правки
    Input заменены на button
    Подключены минимизированный файл стилей и скрипт (пока без автосортировки, она почему-то нарушает стилизацию)

--- js/script.js
+++ js/script.js
@@ -1,68 +1,67 @@
+var button = document.querySelector(".search-form-toggle");
 
-      var button = document.querySelector(".search-form-toggle");
+var modal = document.querySelector(".hotel-search");
 
-      var modal = document.querySelector(".hotel-search");
+var form = modal.querySelector(".hotel-search-form");
 
-      var form = modal.querySelector(".hotel-search-form");
+var checkin = modal.querySelector("[name=checkin]");
 
-      var checkin = modal.querySelector("[name=checkin]");
+var checkout = modal.querySelector("[name=checkout]");
 
-      var checkout = modal.querySelector("[name=checkout]");
+var adults = modal.querySelector("[name=adults]");
 
-      var adults = modal.querySelector("[name=adults]");
+var kids = modal.querySelector("[name=kids]");
 
-      var kids = modal.querySelector("[name=kids]");
+var isStorageSupport = true;
+var storage = "";
 
-      var isStorageSupport = true;
-      var storage = "";
+try {
+  storage = localStorage.getItem("checkin");
+} catch (err) {
+  isStorageSupport = false;
+}
 
-      try {
-        storage = localStorage.getItem("checkin");
-      } catch (err) {
-        isStorageSupport = false;
-      }
+try {
+  storage = localStorage.getItem("checkout");
+} catch (err) {
+  isStorageSupport = false;
+}
 
-      try {
-        storage = localStorage.getItem("checkout");
-      } catch (err) {
-        isStorageSupport = false;
-      }
+try {
+  storage = localStorage.getItem("adults");
+} catch (err) {
+  isStorageSupport = false;
+}
 
-      try {
-        storage = localStorage.getItem("adults");
-      } catch (err) {
-        isStorageSupport = false;
-      }
+try {
+  storage = localStorage.getItem("kids");
+} catch (err) {
+  isStorageSupport = false;
+}
 
-      try {
-        storage = localStorage.getItem("kids");
-      } catch (err) {
-        isStorageSupport = false;
-      }
+button.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modal.classList.toggle("hide");
+  modal.classList.toggle("show");
+});
 
-      button.addEventListener("click", function (evt) {
-        evt.preventDefault();
-        modal.classList.toggle("hide");
-        modal.classList.toggle("show");
-      });
+if (storage) {
+checkin.value = storage;
+checkout.value = storage;
+adults.value = storage;
+kids.value = storage;
+}
 
-      if (storage) {
-      checkin.value = storage;
-      checkout.value = storage;
-      adults.value = storage;
-      kids.value = storage;
+form.addEventListener("submit", function (evt) {
+  if (!checkin.value || !checkout.value || !adults.value || kids.value) {
+    evt.preventDefault();
+    console.log("пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ");
+  } else {
+    if (isStorageSupport) {
+    localStorage.setItem("checkin", checkin.value);
+    localStorage.setItem("checkout", checkout.value);
+    localStorage.setItem("adults", adults.value);
+    localStorage.setItem("kids", kids.value);
     }
-
-      form.addEventListener("submit", function (evt) {
-        if (!checkin.value || !checkout.value || !adults.value || kids.value) {
-          evt.preventDefault();
-          console.log("Укажите даты и количество гостей");
-        } else {
-          if (isStorageSupport) {
-          localStorage.setItem("checkin", checkin.value);
-          localStorage.setItem("checkout", checkout.value);
-          localStorage.setItem("adults", adults.value);
-          localStorage.setItem("kids", kids.value);
-          }
-        }
-      });
+  }
+});
